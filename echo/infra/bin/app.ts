3f0fe87a6016ff9cd0f4470c79c46fe6ae735dc0
Sean Myers <iph@users.noreply.github.com>
2023-12-07 05:21:09 +0000

    Fixup code

--- echo/infra/bin/app.ts
+++ echo/infra/bin/app.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import { EchoStack } from '../lib/echo-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'EchoStack', {
+new EchoStack(app, 'EchoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
